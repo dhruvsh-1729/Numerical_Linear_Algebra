b742392c8a71813584b4d40770d7be6b120a2c86
dhruvsh-1729 <20je0326@mech.iitism.ac.in>
2023-04-20 18:26:27 +0530

    commiting gaussian elimination

---
+++ gaussian_elmn_with_partial_pivoting.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n,i,j,k,temp;
+    float a[10][10],c,d[10]={0};
+
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=n;j++)
+            cin>>a[i][j];
+    }
+	// partial pivoting
+	
+    for(i=n-1;i>0;i--)        
+    {
+        if(a[i-1][0]<a[i][0])
+            for(j=0;j<=n;j++)
+            {
+                c=a[i][j];
+                a[i][j]=a[i-1][j];
+                a[i-1][j]=c;
+            }
+    }
+    //DISPLAY MATRIX
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=n;j++)
+            printf("%6.1f",a[i][j]);
+        printf("\n");
+    }
+    
+    //changing to upper triangular matrix
+    //Forward elimination process
+    
+    for(k=0;k<n-1;k++)
+        for(i=k;i<n-1;i++)
+        {
+            c= (a[i+1][k]/a[k][k]) ;
+
+            for(j=0;j<=n;j++)
+                a[i+1][j]-=c*a[k][j];
+        }
+
+     // DISPLAYING UPPER TRIANGULAr MATRIX
+
+    printf("\n\n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=n;j++)
+            printf("%6.1f",a[i][j]);
+
+        printf("\n");
+	}
+	
+    //Backward Substitution method
+
+    for(i=n-1;i>=0;i--)
+    {
+        c=0;
+        for(j=i;j<=n-1;j++)
+            c=c+a[i][j]*d[j];
+
+        d[i]=(a[i][n]-c)/a[i][i];
+    }
+
+	// DISPLAY
+    
+    for(i=0;i<n;i++)
+    cout<<d[i]<<endl;
+
+    return 0;
+
+}
